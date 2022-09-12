d3f213ee91456f1d9d10715305f4e39cb997cd56
hardik6869 <sorthiyahardik6869@gmail.com>
2022-09-12 15:08:29 +0530

    get Posts Data & Parse Frontmatter

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
 import Layout from "../components/Layout";
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(posts);
   return (
     <Layout>
       <h1>NextJs Blog Site</h1>
     </Layout>
   );
 }
+
+export const getStaticProps = () => {
+  const files = fs.readdirSync(path.join("posts"));
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+    const { data: frontmatter } = matter(markdownWithMeta);
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+  return {
+    props: {
+      posts,
+    },
+  };
+};
