21205b023620de827bf7474bc959b0ed9e0ed175
hardik6869 <sorthiyahardik6869@gmail.com>
2022-09-13 14:49:41 +0530

    Generate Static paths & single Post Data

---
+++ pages/blog/[slug].js
@@ -0,0 +1,39 @@
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+
+const PostPage = ({ frontmatter, content, slug }) => {
+  const { title, category, data, cover_image, author, author_image } =
+    frontmatter;
+  return;
+};
+
+export const getStaticPaths = async () => {
+  const files = fs.readdirSync(path.join("posts"));
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const markdownWithMeta = fs.readFileSync(
+    path.join("posts", slug + ".md"),
+    "utf-8"
+  );
+  const { data: frontmatter, content } = matter(markdownWithMeta);
+  return {
+    props: {
+      frontmatter,
+      content,
+      slug,
+    },
+  };
+};
+
+export default PostPage;
