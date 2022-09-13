54a4bb9184b45a477e6dace4bb4715a0ad91e442
hardik6869 <sorthiyahardik6869@gmail.com>
2022-09-13 15:49:56 +0530

    Display Single Blog Post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,11 +1,39 @@
 import fs from "fs";
 import matter from "gray-matter";
 import path from "path";
+import Link from "next/link";
+import CategoryLabel from "../../components/CategoryLabel";
+import { marked } from "marked";
 
 const PostPage = ({ frontmatter, content, slug }) => {
-  const { title, category, data, cover_image, author, author_image } =
+  const { title, category, date, cover_image, author, author_image } =
     frontmatter;
-  return;
+  return (
+    <div>
+      <Link href="/blog"> Go Back</Link>
+      <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+        <div className="flex justify-between items-center mt-4">
+          <h1 className="text-5xl mb-7"> {title} </h1>
+          <CategoryLabel> {category} </CategoryLabel>
+        </div>
+        <img src={cover_image} alt="" className="w-full rounded" />
+        <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
+          <div className="flex item-center">
+            <img
+              src={author_image}
+              alt=""
+              className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+            />
+            <h4> {author} </h4>
+          </div>
+          <div className="mr-4 "> {date} </div>
+        </div>
+        <div className="blog-test mt-2">
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export const getStaticPaths = async () => {
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,45 @@
-const Blog = () => {
-  return <div>Blog</div>;
-};
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+import Link from "next/link";
+import Post from "../../components/Post";
+import { sortByDate } from "../../utils";
 
-export default Blog;
+export default function BlogPage({ posts }) {
+  return (
+    <div>
+      <h1 className="text-5xl border-b4 p-5 font-bold">Blog</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+      <Link href="/blog">
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+          All Posts
+        </a>
+      </Link>
+    </div>
+  );
+}
+
+export const getStaticProps = () => {
+  const files = fs.readdirSync(path.join("posts"));
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+    const { data: frontmatter } = matter(markdownWithMeta);
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+  return {
+    props: {
+      posts: posts.sort(sortByDate),
+    },
+  };
+};
